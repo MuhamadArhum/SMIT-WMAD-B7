00ade2d6f5299a14d32e026be0f13fc713e44922
MuhamadArhum <muhamadarhum425@gmail.com>
2024-05-01 16:56:24 +0500

    New Files Added Here

--- Assignments/Assignment No 9/script.js
+++ Assignments/Assignment No 9/script.js
@@ -1,7 +1,6 @@
 // Assignment No 9 (This Assignment is focused on string problems in Javascript)
 
 
-
 // a: Return the length of the String.
 // function calculateLength(completeName){
 
@@ -45,20 +44,185 @@
 // checkEmptyorNot(str);
 
 
+
 // d: Count the number of vowels in a string.
+// function countVowels(fullName){
+
+//     fullName = fullName.toLowerCase();
+
+//     var count = 0;
+//     for(var i=0; i<fullName.length; i++){
+//         if(fullName[i] == "a" ||fullName[i] == "e"||fullName[i] == "i"||fullName[i] == "o"||fullName[i] == "u"){
+//             count ++;
+
+//         }
+//     }
+//     return count;
+// }
+
+// let fullName = prompt("Enter Your Name");
+// console.log("The Number of Vowels in given String:",countVowels(fullName));
+
+
+
+
 // e: Reverse a given string.
+// function reverseString(str){
+
+//     return str.split('').reverse().join('');
+
+// }
+
+// let str = prompt("Enter Any Text");
+// console.log("Reversed string:", reverseString(str));
+
+
+
+
+
+
+
 // f: Check if a string is a palindrome.
+
+
+
+
+
+
+
 // g: Convert a string to uppercase.
+// function converttoUpperCase(str){
+//     str = str.toUpperCase();
+
+//     return str;
+// }
+
+// const str = "Hi there ,i'm arhum waheed.";
+// console.log("The Converted Sting is:" + converttoUpperCase(str));
+
+
+
+
+
+
 // h: Find the first occurrence of a given character in a string.
+
+
+
+
+
+
+
 // i: Replace all occurrences of a given character in a string with another character.
+
+
+
+
+
+
 // j: Trim leading and trailing whitespace from a string.
+
+
+
+
+
+
 // k: Count the number of words in a string.
+// function countWords(str){
+
+//     var words = str.split(/\s+/);
+
+//     return words.length;
+// }
+// const str = "Hi there,This Function is used to count the words in GIven String"; 
+// console.log("The Number of Words in this String is:" + countWords(str));
+
+
+
+
 // l: Check if a string contains only numeric characters.
+
+
+
+
+
+
+
+
+
 // m: Check if a string is a valid email address.
+// function checkEmailValid(Email){
+
+
+//     // Pattern Initialized:
+//     var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     // Return the Function.
+//     return pattern.test(Email);
+
+
+// }
+
+// let Email = prompt("Enter Your Email Address Here");
+// console.log(checkEmailValid(Email));
+
+
 // n: Extract the domain name from a URL string.
+
+
+
+
+
+
+
+
+
 // o: Convert a string into title case (the first letter of each word capitalized).
+
+
+
+
+
+
+
+
+
 // p: Remove all non-alphabetic characters from a string.
+
+
+
+
+
+
+
+
+
 // q: Check if a string is a valid palindrome permutation (ignoring spaces).
+
+
+
+
+
+
+
+
+
 // r: Check if a string is an anagram of another string.
+
+
+
+
+
+
+
+
+
 // s: Find the longest word in a string.
+
+
+
+
+
+
+
+
 // t: Perform string compression by replacing repeated characters with the character followed by the count.
