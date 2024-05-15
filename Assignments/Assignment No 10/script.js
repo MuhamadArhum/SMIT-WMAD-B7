60655dfcca2e69262ef089faef77f3e53c172654
MuhamadArhum <muhamadarhum425@gmail.com>
2024-05-16 02:24:05 +0500

    new

--- Assignments/Assignment No 10/script.js
+++ Assignments/Assignment No 10/script.js
@@ -1,13 +1,32 @@
 // Assignment No 10 (JavaScript Objects)
 
 // Task No 1: (Create a Object).
-let student = {
+let person = {
     name:'Ali Hassan',
     age:10,
-    profession:'study',
+    profession:'software Engineer',
+
+
+    // Task No 3
+    introduce: function(){
+        console.log("Hi I'm",this.name," My age is",this.age," I am a",this.profession);
+
+    }
 }
 
+person.introduce();
 // Task No 2: (Print the Values of Object).
-console.log("The New Value of Age is:"+ student.age);
+console.log("The Name of Person is:"+ person.name);
+console.log("The Age of Person is:"+ person.age);
+console.log("The Profession of Person is:"+ person.profession);
 
-// Task No 3: (Create JS Object Methods).
+// Modify the Value.
+person.age = 18;
+console.log("The Updated Age of Person is:"+ person.age);
+
+// Add new Property in Person Object.
+person.address = 'Faisalabad';
+console.log("The Address of the Person is:" + person.address);
+
+
+
--- Assignments/Assignment No 11/script.js
+++ Assignments/Assignment No 11/script.js
@@ -2,38 +2,54 @@
 
 // JS Strings (All Methods of Strings).
 var abc = "hi tehteew iashjdhsuiywefu";
-console.log(abc);
+// console.log(abc);
 
 var esc = 'I dont \n know who i am';
-console.log(esc);
+// console.log(esc);
 
 var len = abc.length;
-console.log(len);
+// console.log(len);
 
-console.log(abc.indexOf("teh"));
-console.log(abc.lastIndexOf("teh"));
-console.log(abc.slice(3,10));
-console.log(abc.replace("hi","HI"));
-console.log(abc.toUpperCase());
-console.log(abc.toLowerCase());
-console.log(abc.concat(" ", esc));
-console.log(abc.charAt(3));
-console.log(abc[5]);
-console.log(abc.charCodeAt(4));
-console.log(abc.split(","));
-console.log(abc.split(""));
+// console.log(abc.indexOf("teh"));
+// console.log(abc.lastIndexOf("teh"));
+// console.log(abc.slice(3,10));
+// console.log(abc.replace("hi","HI"));
+// console.log(abc.toUpperCase());
+// console.log(abc.toLowerCase());
+// console.log(abc.concat(" ", esc));
+// console.log(abc.charAt(3));
+// console.log(abc[5]);
+// console.log(abc.charCodeAt(4));
+// console.log(abc.split(","));
+// console.log(abc.split(""));
 
 
 
 // JS Arrays (All Methods of Arrays).
 var num = [1,2,3,4];
-console.log(num.at(1));
-console.log(num.push(6));
-console.log(num.pop(),num);
-console.log(num.fill(1));
-
+// console.log(num.at(1));
+// console.log(num.push(6));
+// console.log(num.pop(),num);
+// console.log(num.fill(1));
+// console.log(num.shift());
+// console.log(num.unshift(5));
+// console.log(num);
+// console.log(num.reverse());
+// console.log(num.includes(6));
+// console.log(num.map(num => 2*num));
+// console.log(num.filter(num => num>2));
+// console.log(num.find(num => num>2));
+// console.log(num.every(num=> num>0));
+// console.log(num.findIndex(num => num===1));
+// console.log(num.reduce()); This Will Check after.
+// console.log(num.toString());
+// console.log(num.join("*"));
+// console.log(num.splice(2,3,"i","p"));This will also check.
+// console.log(num.slice(1,2));
+ 
 
 
 
 // JS Numbers (All Methods of Numbers).
+
 // JS Math (All Methods of Math).
