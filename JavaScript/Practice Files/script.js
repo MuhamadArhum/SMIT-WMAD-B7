53b2c0c65e0a2b636b294f574a3a86a14da8b001
MuhamadArhum <muhamadarhum425@gmail.com>
2024-05-19 17:26:14 +0500

    New Update

--- JavaScript/Practice Files/script.js
+++ JavaScript/Practice Files/script.js
@@ -1,9 +1,19 @@
+// Change Color of 1st Button
 
-// Same Function as HTML Inline JS.
+function changeColor1(){
+    document.getElementById('Button1').style.backgroundColor = 'Blue';
+}
+
+// Change Color of 2nd Button.
 function changeColor(){
-    document.querySelector('button').style.backgroundColor = 'green';
+    document.getElementById('Button2').style.backgroundColor = 'green';
 }
 
 function resetColor(){
-    document.querySelector('button').style.backgroundColor = '';
+    document.getElementById('Button2').style.backgroundColor = '';
+}
+
+// Change Color Double Click.
+function changeColor3(){
+    document.getElementById('Button3').style.color = 'Yellow';
 }
