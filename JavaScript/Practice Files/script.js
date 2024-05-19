28cc2767422c499698d38e0ef7b24da288747b79
MuhamadArhum <muhamadarhum425@gmail.com>
2024-05-19 16:35:56 +0500

    new

---
+++ JavaScript/Practice Files/script.js
@@ -0,0 +1,9 @@
+
+// Same Function as HTML Inline JS.
+function changeColor(){
+    document.querySelector('button').style.backgroundColor = 'green';
+}
+
+function resetColor(){
+    document.querySelector('button').style.backgroundColor = '';
+}
