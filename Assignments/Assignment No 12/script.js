a6a91a0c2584425789d3309cd51254b21595aedb
MuhamadArhum <muhamadarhum425@gmail.com>
2024-06-22 13:36:12 +0500

    New Project

---
+++ Assignments/Assignment No 12/script.js
@@ -0,0 +1,21 @@
+
+
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '0';
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+
+    // Eval Function is Evaluate the Given Input and Slve it According to the Given Operation.
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
