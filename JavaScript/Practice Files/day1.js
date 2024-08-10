68f432136608158c4405aed287f76671ee03e562
MuhamadArhum <muhamadarhum425@gmail.com>
2024-08-10 07:44:56 +0500

    New

---
+++ JavaScript/Practice Files/day1.js
@@ -0,0 +1,39 @@
+// // Test 1
+// function addTwoNum(num){
+//     console.log(num);
+// }
+// let a=100 , b=12;
+// let sum = a + b;
+// let num = sum;    // Replace the names of variables:
+// addTwoNum(num);  // Call the Function
+
+
+// Test 2
+// function evenORodd(num){
+//     if(num % 2 == 0){
+//         console.log("The",num,"is Even");
+//     }
+//     else{
+//         console.log("The",num,"is odd");
+//     }
+// }
+// let num = parseInt(prompt("Enter Your 1st Number:"));
+// evenORodd(num); //Call the Function.
+
+
+
+// Test 3
+// function sumOfArr(){
+//     let number1 = parseInt(prompt("Enter Number 1"));
+//     let number5 = parseInt(prompt("Enter Number 2"));
+//     let number2 = parseInt(prompt("Enter Number 3"));
+//     let number3 = parseInt(prompt("Enter Number 4"));
+//     let number4 = parseInt(prompt("Enter Number 5"));
+//     let asd = [number1,number2,number3,number4,number5];
+//     let sum = 0;
+//     for(let i=0; i<asd.length;i++){
+//         sum += asd[i];
+//     }
+//     console.log(sum);    
+// }
+// sumOfArr(); // Call the Function
